32614419532877cc0ee41017f8a205d8fada6b5d
gaoconghui <381147882@qq.com>
2018-01-14 18:57:46 +0800

    add rate test

---
+++ run_test_huobi_rate.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import logging
+import logging.handlers
+import sys
+import time
+
+import os
+
+from trader import password
+from trader.deal import HuobiTrader
+
+_video_sch_dir = '%s/' % os.path.dirname(os.path.realpath(__file__))
+_filepath = os.path.dirname(sys.argv[0])
+sys.path.insert(1, os.path.join(_filepath, _video_sch_dir))
+
+logger = logging.getLogger(__name__)
+
+
+def init_log():
+    log_path = "./trader_test.log"
+    fh = logging.handlers.TimedRotatingFileHandler(
+        filename=log_path, when='midnight')
+    fh.suffix = "%Y%m%d-%H%M.log"
+    fh.setLevel(logging.INFO)
+
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+
+    # create formatter
+    fmt = "%(asctime)s.%(msecs)03d %(filename)s %(message)s"
+    datefmt = "%a %d %b %Y %H:%M:%S"
+    formatter = logging.Formatter(fmt, datefmt)
+
+    # add handler and formatter to logger
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    logger.setLevel(logging.INFO)
+
+
+if __name__ == '__main__':
+    init_log()
+    # 火币网数据获取
+    trader = HuobiTrader(access_key=password.access_key, secret_key=password.secret_key)
+    for i in range(10):
+        t1 = time.time()
+        trader.get_balance()
+        logger.info("test trader get balalce time {t}".format(t=time.time() - t1))
