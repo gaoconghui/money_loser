3ee8c8fe3f9fb97f8730068e4fd5f431570672ab
gaoconghui <381147882@qq.com>
2018-01-19 16:51:59 +0800

    update strategy two

--- trader_v2/strategy/strategy_two.py
+++ trader_v2/strategy/strategy_two.py
@@ -16,6 +16,7 @@ class StrategyTwo(StrategyBase):
     二号策略
     kline 获取，huobi给的跟自己算的有点出入，但大体差的不多。
     启动的时候会获取一次kline，怕之后自己算的跟一开始启动时候获取的有出入，决定都使用huobi给的kline，如果碰到问题了再说好了
+    回测结果：没有手续费的时候效果不错，但是手续费太高了，加完手续费亏损太多。
     """
 
     __name__ = "strategy two"
@@ -34,6 +35,7 @@ class StrategyTwo(StrategyBase):
         self.array_managers = {symbol: ArrayManager() for symbol in symbols}
 
         self.running = True
+        self.can_trade = False
 
     def start(self):
         StrategyBase.start(self)
@@ -51,19 +53,32 @@ class StrategyTwo(StrategyBase):
         symbol = bars[0].symbol
         for bar in bars:
             self.bar_managers[symbol].update_from_bar(bar)
+        self.can_trade = True
 
     def on_bar(self, symbol, bar):
         self.array_managers[symbol].update_bar(bar)
         self.check(symbol)
 
     def check(self, symbol):
-        if self.running:
-            print self.bar_managers[symbol].bar
+        """
+        当出现金叉买进，死叉卖出
+        :param symbol: 
+        :return: 
+        """
+        if self.running and self.can_trade:
             _, _, hist = self.array_managers[symbol].macd(fast_period=12, slow_period=26, signal_period=9, array=True)
-            if hist[-2] < 0 < hist[-1]:
+            # 金叉
+            # if hist[-2] < 0 < hist[-1]:
+            # 底背离
+            if hist[-1] > hist[-2] and hist[-3] > hist[-2]:
                 self.strategy_engine.limit_buy(symbol=symbol, price=self.bar_managers[symbol].bar.close)
-            if hist[-1] < 0 < hist[-2]:
+                # self.can_trade = False
+            # 死叉
+            # if hist[-1] < 0 < hist[-2]:
+            # 顶背离
+            if hist[-1] < hist[-2] and hist[-3] < hist[-2]:
                 self.strategy_engine.limit_sell(symbol=symbol, price=self.bar_managers[symbol].bar.close)
+                # self.can_trade = False
 
     def stop(self):
         StrategyBase.stop(self)
