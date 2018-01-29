93afcc05223bd2a8021d940d89271f63a970f252
gaoconghui <381147882@qq.com>
2018-01-29 21:02:49 +0800

    dump kline in mongo

--- trader_v2/api_wrapper.py
+++ trader_v2/api_wrapper.py
@@ -4,9 +4,12 @@
 """
 import pandas as pd
 from pandas import DataFrame
+from pymongo import MongoClient
 
 from trader_v2 import api
 
+
+# def get_kline_from_mongo(symbol,period,size)
 
 def get_kline_df(symbol, period, size):
     bars = api.get_kline(symbol, period, size)['data'][::-1]
@@ -17,5 +20,38 @@ def get_kline_df(symbol, period, size):
     return df
 
 
+coll = None
+
+
+def init_mongo(coll_name):
+    global coll
+    client = MongoClient(host="localhost")
+    db = client['huobi']
+    coll = db[coll_name]
+    return coll
+
+
+def get_kline_from_mongo(symbol, period, size):
+    coll = init_mongo("kline_" + period)
+    return list(coll.find({"symbol" : symbol}).sort([("ts" , -1)]).limit(size))
+
+
+def dump_kline(symbol, period, size, overload=True):
+    coll = init_mongo("kline_" + period)
+    print coll
+    bars = api.get_kline(symbol, period, size)['data'][::-1]
+    for bar in bars:
+        _id = bar.pop("id")
+        bar["_id"] = symbol + "_" + str(_id)
+        bar["ts"] = _id
+        bar["period"] = period
+        bar["symbol"] = symbol
+        if coll.find_one({"_id": bar["_id"]}):
+            if overload:
+                coll.update_one({"_id": bar["_id"]}, {"$set": bar})
+        else:
+            coll.insert_one(bar)
+
+
 if __name__ == '__main__':
-    print get_kline_df("waxeth", "1min", 100)
+    print dump_kline("swftcbtc", "15min", 2000)
